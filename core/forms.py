b90867287ed90f97b895ef953a984046876768d5
dieraTEC <dieraTEC@estudiantec.cr>
2023-04-09 01:08:47 -0600

    Cambios visuales en formulario de crear cuenta

--- core/forms.py
+++ core/forms.py
@@ -1,16 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import password_validation
 
 from .models import User
 
 
 class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'input'}),
+        label="Contraseña:",
+        help_text=password_validation.password_validators_help_text_html()
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'input'}),
+        label="Confirmar contraseña:",
+        help_text="Para verificar, introduzca la misma contraseña anterior."
+    )
 
     class Meta:
         model = User
         fields = ("username", )
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'input'})
+            'username': forms.TextInput(attrs={'class': 'input'}),
         }
 
 
